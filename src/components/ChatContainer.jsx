719b268af5e595b44f15159a9765c19f73ef1e87
Rafi <md.rafi888444@gmail.com>
2025-07-23 00:49:21 +0600

    chat done

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,41 +1,77 @@
-import React, { useEffect } from 'react';
-import { useChatStore } from '../store/useChatStore';
-import ChatHeader from './ChatHeader';
-import MessageInput from './MessageInput';
-import MessageSkeleton from '../pages/skeletons/MessageSkeleton';
+import React, { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
+import MessageSkeleton from "../pages/skeletons/MessageSkeleton";
+import { useAuthStore } from "../store/useAuthStore";
+import { Timer } from "lucide-react";
+import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = () => {
+  const { messages, getMessages, isMessageLoading, selectedUser } =
+    useChatStore();
+  const { authUser } = useAuthStore();
 
-    const {messages, getMessages, isMessageLoading, selectedUser} = useChatStore()
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
 
-
-  useEffect(()=>{
-    getMessages(selectedUser._id)
-  },[selectedUser._id,getMessages])
-
-
-  
-
-  if(isMessageLoading) return (
-    <div>
+  if (isMessageLoading)
+    return (
+      <div>
         <ChatHeader></ChatHeader>
         <MessageSkeleton></MessageSkeleton>
         <MessageInput></MessageInput>
-    </div>
-  )
-  else{
- return (
-        <div className='flex-1 flex flex-col overflow-auto'>
-            <ChatHeader></ChatHeader>
+      </div>
+    );
+  else {
+    return (
+      <div className="flex-1 flex flex-col overflow-auto h-full">
+        <ChatHeader></ChatHeader>
 
-            <p>messages...</p>
-            <MessageInput></MessageInput>
-            
+        <div className="flex-1 overflow-y-auto p-4 space-y-4 h-screen">
+          {messages.map((message) => (
+            <div
+              key={message._id}
+              className={`chat ${
+                message.senderId === authUser._id ? "chat-end" : "chat-start"
+              }`}
+            >
+              <div className="chat-image avatar">
+                <div className="size-10 rounded-full border">
+                  <img
+                    src={
+                      message.senderId === authUser._id
+                        ? authUser.profilePic || "/avatar.jpg"
+                        : selectedUser.profilePic || "/avatar.jpg"
+                    }
+                    alt="profile pic"
+                  />
+                </div>
+              </div>
+              <div className="chat-header mb-1">
+                <time className="text-sx opacity-50 ml-1">{formatMessageTime(message.createdAt)}</time>
+                 
+              </div>
+              <div className="chat-bubble flex flex-col">
+                {message.image && (
+                    <img
+                    src={message.image}
+                    alt="pic"
+                    className="sm:max-w-[200px] rounded-md mb-2"
+                    
+                    />
+                )}
+                 {message.text && <p>{message.text}</p>}
+              </div>
+            </div>
+          ))}
         </div>
+
+        <MessageInput></MessageInput>
+      </div>
     );
   }
-
-   
 };
 
 export default ChatContainer;
